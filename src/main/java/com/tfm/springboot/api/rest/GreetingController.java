6e333d1fe652845b7a3e8ef07eed6914b7899cce
Raul <raulmuve@gmail.com>
2020-12-02 11:14:09 +0100

    Creacion Modelos
    Creamos los modelos para la entradas/salidas de las llamadas al servicio
    de rutas y refugios

--- src/main/java/com/tfm/springboot/api/rest/GreetingController.java
+++ src/main/java/com/tfm/springboot/api/rest/GreetingController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tfm.springboot.entity.Ruta;
 import com.tfm.springboot.rest.model.Greeting;
 import com.tfm.springboot.rest.model.RutesSearchOutputRuta;
 
---
+++ src/main/java/com/tfm/springboot/service/impl/RutaServiceImpl.java
@@ -0,0 +1,39 @@
+package com.tfm.springboot.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.tfm.springboot.entity.Ruta;
+import com.tfm.springboot.rest.model.RutaPostInput;
+import com.tfm.springboot.rest.model.RutaPostOutput;
+import com.tfm.springboot.rest.model.RutesSearchOutputRuta;
+import com.tfm.springboot.service.RutaService;
+
+@Service
+public class RutaServiceImpl implements RutaService {
+
+	public RutaPostOutput AddRuta(RutaPostInput ruta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RutaPostOutput ModifyRuta(Ruta ruta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RutesSearchOutputRuta searchByCim(int idCim) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RutesSearchOutputRuta searchByRefugi(int idRefugi) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public RutesSearchOutputRuta topTen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
