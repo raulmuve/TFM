79767a38c189a0671fe85f4ec85cd135fac77d3d
Raul <raulmuve@gmail.com>
2020-12-02 11:59:28 +0100

    Fix RutaService
    Cambiamos el tipo de parametro en la interficie rutaservice y en la
    implementacion de los metodos add y modify

--- src/main/java/com/tfm/springboot/service/RutaService.java
+++ src/main/java/com/tfm/springboot/service/RutaService.java
@@ -1,14 +1,14 @@
 package com.tfm.springboot.service;
 
-import com.tfm.springboot.entity.Ruta;
+import com.tfm.springboot.rest.model.RutaPostInput;
 import com.tfm.springboot.rest.model.RutaPostOutput;
 import com.tfm.springboot.rest.model.RutesSearchOutputRuta;
 
 public interface RutaService {
 
-	RutaPostOutput AddRuta(Ruta ruta);
+	RutaPostOutput AddRuta(RutaPostInput ruta);
 
-	RutaPostOutput ModifyRuta(Ruta ruta);
+	RutaPostOutput ModifyRuta(RutaPostInput ruta);
 
 	RutesSearchOutputRuta searchByCim(int idCim);
 
--- src/main/java/com/tfm/springboot/service/impl/RutaServiceImpl.java
+++ src/main/java/com/tfm/springboot/service/impl/RutaServiceImpl.java
@@ -2,7 +2,6 @@ package com.tfm.springboot.service.impl;
 
 import org.springframework.stereotype.Service;
 
-import com.tfm.springboot.entity.Ruta;
 import com.tfm.springboot.rest.model.RutaPostInput;
 import com.tfm.springboot.rest.model.RutaPostOutput;
 import com.tfm.springboot.rest.model.RutesSearchOutputRuta;
@@ -16,7 +15,7 @@ public class RutaServiceImpl implements RutaService {
 		return null;
 	}
 
-	public RutaPostOutput ModifyRuta(Ruta ruta) {
+	public RutaPostOutput ModifyRuta(RutaPostInput ruta) {
 		// TODO Auto-generated method stub
 		return null;
 	}
